ee798a10b6fc0b97bdeed06eaae3bcaf39bc3cda
unknown <waddellnathan1211@gmail.com>
2021-01-20 22:47:11 -0600

    Fixing main page styling

--- presentation/components/main-page/main-page.styles.tsx
+++ presentation/components/main-page/main-page.styles.tsx
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
 
 export const MainPageWrapper = styled.div`
-    width: 100%;
+    margin: 0 auto;
+    margin-top: 5rem;
+    max-width: 1200px;
+    width: 88%;
     height: 85vh;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 80%;
+    align-items: center;
+    margin: 20px 0;
+    > input {
+        width: 45%;
+        margin-right: 20px;
+    }
+    > div {
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 5%;
+        height: 80%;
+        border-radius: 4px;
+        cursor: pointer;
+    }
 `;
