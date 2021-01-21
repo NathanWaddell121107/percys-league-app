458159899fce3e1432e70482fac3996bab2092b8
unknown <waddellnathan1211@gmail.com>
2021-01-20 22:48:18 -0600

    Package fixes / config changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  semi: false,
-  tabWidth: 2,
-  useTabs: true,
-  singleQuote: true,
-  printWidth: 120,
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  arrowParens: "always",
-  trailingComma: "none",
-};
+	semi: false,
+	tabWidth: 1,
+	useTabs: true,
+	singleQuote: true,
+	printWidth: 80,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always',
+	trailingComma: 'none'
+}
