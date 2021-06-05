8afb889203f1d2787427c3a790743f0d0e1fe3d0
unknown <waddellnathan1211@gmail.com>
2021-06-04 23:36:17 -0500

    Matches component

---
+++ presentation/components/matches/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import * as Styled from './matches.styles'
+import { Player } from '../../interfaces/player'
+import LoadingIndicator from '../loading-indicator'
+import { fetchPlayers } from '../util/player-methods'
+import SelectPlayers from './select-players'
+import { Button } from 'reactstrap'
+
+const Matches: React.FC = () => {
+	const [selectPlayersModalIsOpen, setSelectPlayersModalIsOpen] = React.useState(
+		true
+	)
+	const [selectedPlayers, setSelectedPlayers] = React.useState<Array<Player>>([])
+	const [playersList, setPlayersList] = React.useState<Array<Player>>()
+
+	React.useEffect(() => {
+		const getPlayers = async () => {
+			const { players, success, error } = await fetchPlayers()
+			if (!success) {
+				if (error) alert('Uh-oh, couldn`t retrieve the players list')
+				else
+					console.log(
+						'looks like there may not be any players yet. Double check the players collection'
+					)
+			} else if (players && players.length > 0) {
+				setPlayersList(players)
+			}
+		}
+		getPlayers()
+	}, [])
+
+	if (!playersList) {
+		return <LoadingIndicator />
+	}
+
+	return (
+		<>
+			{selectPlayersModalIsOpen && (
+				<SelectPlayers
+					selectPlayersModalIsOpen={selectPlayersModalIsOpen}
+					setSelectPlayersModalIsOpen={setSelectPlayersModalIsOpen}
+					players={playersList}
+					setSelectedPlayers={setSelectedPlayers}
+					selectedPlayers={selectedPlayers}
+				/>
+			)}
+			{!selectedPlayers && (
+				<Styled.SelectPlayersMessage>
+					<span>Please select the players you would like to create games for</span>
+					<Button
+						onClick={() => setSelectPlayersModalIsOpen(true)}
+						color="secondary">
+						Select
+					</Button>
+				</Styled.SelectPlayersMessage>
+			)}
+		</>
+	)
+}
+
+export default Matches
