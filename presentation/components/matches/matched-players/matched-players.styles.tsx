769a23422627a9c00bd318be71b79d371572fdb3
unknown <waddellnathan1211@gmail.com>
2021-06-05 01:02:55 -0500

    Adding matched players ui/logic

---
+++ presentation/components/matches/matched-players/matched-players.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const MatchedPlayersWrapper = styled.div`
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const TitleDiv = styled.div`
+	margin: 1rem 0;
+`
